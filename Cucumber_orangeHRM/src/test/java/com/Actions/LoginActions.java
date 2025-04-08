a4f0ad8eecec6a5cfce25fec29a1f3a4f12279ef
Sevvelkaran <2k21it50@kiot.ac.in>
2025-04-08 21:58:32 +0530

    Day - 1

---
+++ Cucumber_orangeHRM/src/test/java/com/Actions/LoginActions.java
@@ -0,0 +1,10 @@
+package com.Actions;
+
+public class LoginActions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Cucumber_orangeHRM/src/test/java/com/Pages/LoginPages.java
@@ -0,0 +1,10 @@
+package com.Pages;
+
+public class LoginPages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
