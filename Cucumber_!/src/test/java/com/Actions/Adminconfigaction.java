51381a072a06570bbbbd0b87a4240e3497f2de9a
thouxzzh <2k21cse167@gmail.com>
2025-05-01 22:28:05 +0530

    Added the Language modification in Z-Adminconfigfeature

--- Cucumber_!/src/test/java/com/Actions/Adminconfigaction.java
+++ Cucumber_!/src/test/java/com/Actions/Adminconfigaction.java
@@ -3,8 +3,10 @@ package com.Actions;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -40,7 +42,28 @@ public class Adminconfigaction {
    public void verify() {
 	   String expected="Recruitment";
 	   String actual=adcon.text.getText();
-	   Assert.assertNotEquals(expected, actual);;
+	   Assert.assertNotEquals(expected, actual);
+	   clickMethod(adcon.recruitment);
+	   clickMethod(adcon.save);
+   }
+   public void clicklocal() {
+	   clickMethod(adcon.configuration);
+	   wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("spinner")));
+	   clickMethod(adcon.local);
+	   wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("spinner")));
+   }
+   public void selectlang() {
+//	   adcon.lang.sendKeys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ENTER);
+   }
+   public void selectdate() {
+//	   adcon.date.sendKeys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ENTER);
+   }
+   public void clicksubmit() {
+	   clickMethod(adcon.submit);
+	   wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("oxd-loading-spinner")));
+   }
+   public void verifylang(String expected) {
+	   String actual=adcon.lang.getText();
    }
    public void clickMethod(WebElement element) {
  	  wait = new WebDriverWait(driver, Duration.ofSeconds(10));
--- Cucumber_!/src/test/java/com/Definations/AdminconfigDefinitions.java
+++ Cucumber_!/src/test/java/com/Definations/AdminconfigDefinitions.java
@@ -32,4 +32,31 @@ public class AdminconfigDefinitions {
 	public void the_recruitment_module_should_be_hidden_from_the_menu() {
 	   adcon.verify();
 	}
+	
+	//localization
+	@When("the user clicks on Configuration and selects Localization")
+	public void the_user_clicks_on_configuration_and_selects_localization() {
+	    adcon.clicklocal();
+	}
+
+	@When("the user selects {string} as the language")
+	public void the_user_selects_as_the_language(String string) {
+	    adcon.selectlang();
+	}
+
+	@When("the user selects the date format as {string}")
+	public void the_user_selects_the_date_format_as(String string) {
+	    adcon.selectdate();
+	}
+
+	@When("the user click the Save")
+	public void the_user_click_the_save() {
+	   adcon.clicksubmit();
+	}
+
+	@Then("the user should see the interface in the selected language")
+	public void the_user_should_see_the_interface_in_the_selected_language() {
+	   String expected="English (United States)";
+	   adcon.verifylang(expected);
+	}
 }
