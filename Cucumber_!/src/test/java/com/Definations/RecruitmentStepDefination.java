000529c24ba099b2fc1863b619291617999a4060
Sevvelkaran <2k21it50@kiot.ac.in>
2025-05-05 22:17:50 +0530

    Added new feature - Recruitment on 05MAY2025

---
+++ Cucumber_!/src/test/java/com/Definations/RecruitmentStepDefination.java
@@ -0,0 +1,50 @@
+package com.Definations;
+
+import io.cucumber.java.en.*;
+
+public class RecruitmentStepDefination {
+	
+
+@When("The user clicks on the Recruitment tab and clicks on the Candidates page")
+public void the_user_clicks_on_the_recruitment_tab_and_clicks_on_the_candidates_page() {
+   
+}
+
+@When("Clicks on Add")
+public void clicks_on_add() {
+    
+}
+
+@When("The user enters Firstname as {string}, MiddleName as {string} and LastName as {string}")
+public void the_user_enters_firstname_as_middle_name_as_and_last_name_as(String string, String string2, String string3) {
+   
+
+@When("Selects Vacancy")
+public void selects_vacancy() {
+   
+}
+
+@When("The user enters Email as {string} and Contact as {string}")
+public void the_user_enters_email_as_and_contact_as(String string, String string2) {
+    
+}
+
+@When("Enters keyword as {string}")
+public void enters_keyword_as(String string) {
+    
+}
+
+@When("Clicks on Save")
+public void clicks_on_save() {
+    
+}
+
+@Then("Assert to check its presence")
+public void assert_to_check_its_presence() {
+    
+}
+
+
+
+
+}
