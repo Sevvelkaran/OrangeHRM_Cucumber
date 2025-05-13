c6119f0db48c201b274fa29e2cb45ce8bff2aa00
DharaniMurugan01 <2k21dhara@gmail.com>
2025-05-13 15:31:11 +0530

    Update Runner.java

--- Cucumber_!/src/test/java/com/Runner/Runner.java
+++ Cucumber_!/src/test/java/com/Runner/Runner.java
@@ -20,7 +20,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
  	          "src/test/resources/com/features/Performance.feature",
              "src/test/resources/com/features/Performance2.feature",
  	          "src/test/resources/com/features/Buzz.feature",
-	  	      // "src/test/resources/com/features/TimePage.feature",
+	  	       "src/test/resources/com/features/TimePage.feature",
 	  	       "src/test/resources/com/features/Admin2.feature",
 	  	      "src/test/resources/com/features/Z-Adminconfig.feature",
 	  	      "src/test/resources/com/features/SocialMedia.feature",
